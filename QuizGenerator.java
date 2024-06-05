c0ceb499c0fe1915d8360b4df769a89183ae15b3
Harshithabotta <harshitha.botta9@gmail.com>
2024-06-05 13:11:15 +0530

    first commit

--- QuizGenerator.java
+++ QuizGenerator.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class QuizApp {
+public class QuizGenerator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         QuizManager quizManager = new QuizManager();
